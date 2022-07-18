cf02cfd468f777b9532eeff75ee1e438b59614ed
shivam1709 <68727150+shivam1709@users.noreply.github.com>
2022-07-18 11:35:20 -0400

    first commit

---
+++ Expensify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Expensify
+//
+//  Created by user206868 on 7/18/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
