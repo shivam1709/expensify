9f05babe6def7f3dda433d17cd9a2c443cfd1a0b
rensonsunny <rensonsunny@gmail.com>
2022-07-19 23:14:23 -0400

    login and register screens
    Login and register page- partial updates

---
+++ Expensify/LoginScreenController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginScreenController.swift
+//  Expensify
+//
+//  Created by user207261 on 7/19/22.
+//
+
+import UIKit
+
+class LoginScreenController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
