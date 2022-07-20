6233d8fa24539e28a623eb25b623a34e95950db5
Rahul Patel <rvpatel5898@gmail.com>
2022-07-19 22:34:31 -0700

    Home screen changes

--- Expensify/HomeScreenController.swift
+++ Expensify/HomeScreenController.swift
@@ -7,13 +7,27 @@
 
 import UIKit
 
-class HomeScreenController: UIViewController {
-
+class HomeScreenController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
-
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MyCell", for: indexPath)
+        cell.backgroundColor = .cyan
+        return cell
+    }
 
 }
 
