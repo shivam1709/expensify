00761973def2566cee72e21aba32ac695cfe2be5
rensonsunny <rensonsunny@gmail.com>
2022-07-27 09:28:50 -0400

    integrated firebase

--- Expensify/LoginScreenController.swift
+++ Expensify/LoginScreenController.swift
@@ -9,21 +9,17 @@ import UIKit
 
 class LoginScreenController: UIViewController {
 
+    @IBOutlet weak var txtEmail: UITextField!
+    
+    @IBOutlet weak var txtPassword: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func Login(_ sender: Any) {
     }
-    */
-
+    
 }
--- Expensify/RegisterScreenController.swift
+++ Expensify/RegisterScreenController.swift
@@ -9,21 +9,21 @@ import UIKit
 
 class RegisterScreenController: UIViewController {
 
+    
+    @IBOutlet weak var txtName: UITextField!
+    
+    @IBOutlet weak var txtEmail: UITextField!
+    
+    
+    @IBOutlet weak var txtPassword: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func Register(_ sender: Any) {
     }
-    */
-
+    
 }
