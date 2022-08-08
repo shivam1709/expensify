c751f43689009e66b56bad267803da4dc69423c5
Rahul Patel <rvpatel5898@gmail.com>
2022-08-08 12:24:24 -0700

    Updated my screen in latest code for the first time

--- Expensify/HomeScreenController.swift
+++ Expensify/HomeScreenController.swift
@@ -1,20 +1,34 @@
 //
 //  ViewController.swift
-//  Expensify
+//  CollectionView
 //
-//  Created by user206868 on 7/18/22.
+//  Created by user206828 on 8/1/22.
 //
 
 import UIKit
 
-class HomeScreenController: UIViewController {
+class HomeScreenController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    let monthArr:[String] = ["January","February","March","April","May","June", "July", "August", "September", "October", "November", "December"]
 
+    @IBOutlet weak var myCollectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationItem.hidesBackButton = true	
         // Do any additional setup after loading the view.
+        self.myCollectionView.delegate = self
+        self.myCollectionView.dataSource = self
     }
+    
 
-
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return monthArr.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = myCollectionView.dequeueReusableCell(withReuseIdentifier: "myCell", for: indexPath) as! CollectionViewCell
+        
+        cell.monthName.text = monthArr[indexPath.row]
+        return cell
+    }
 }
 
